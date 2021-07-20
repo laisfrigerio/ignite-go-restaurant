6e1e84098dc55d2357d87dee52ec6df98851e97c
Lais Frigério <laiss@gazetadopovo.com.br>
2021-07-20 20:15:49 -0300

    Dashboard as functional component

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,5 @@
 import { Switch, Route } from 'react-router-dom'
-import Dashboard from '../pages/Dashboard'
+import { Dashboard } from '../pages/Dashboard'
 
 export function Routes () {
   return (
